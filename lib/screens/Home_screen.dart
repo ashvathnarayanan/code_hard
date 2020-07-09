be4edc902494a8b622ad2af2c4502574adeb5f14
Hash sathish <55404795+hashsathish@users.noreply.github.com>
2020-07-09 19:37:46 +0530

    Update Home_screen.dart

--- lib/screens/Home_screen.dart
+++ lib/screens/Home_screen.dart
@@ -61,6 +61,7 @@ class _HomePageState extends State<HomePage> {
           bottomNavigationBar: CustomBottomNavigationBar(
             currentIndex: _selectedPageIndex,
             onChange: _selectPage,
+            backgroundColor: Colors.white,
             children: [
               CustomBottomNavigationItem(
                 icon: Icons.home,
