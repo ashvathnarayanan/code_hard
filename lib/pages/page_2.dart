fd3a2b154a590254aa5297593f8672f20183b7f0
Hash sathish <55404795+hashsathish@users.noreply.github.com>
2020-07-09 20:54:57 +0530

    Update page_2.dart

--- lib/pages/page_2.dart
+++ lib/pages/page_2.dart
@@ -2,14 +2,56 @@ import 'package:flutter/material.dart';
 import '../widgets/app_bar.dart';
 import '../widgets/top.dart';
 
+
 class Page2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: homeAppBar(context),
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Center(
+          child: Container(
+            child: RichText(
+              text: TextSpan(
+                style: Theme.of(context).textTheme.headline6.copyWith(
+                      fontWeight: FontWeight.bold,
+                    ),
+                children: [
+                  TextSpan(
+                    text: "EM",
+                    style: TextStyle(
+                      color: Colors.blue[300],
+                    ),
+                  ),
+                  TextSpan(
+                    text: "PLOYED",
+                    style: TextStyle(
+                      color: Colors.grey[200],
+                    ),
+                  ),
+                  TextSpan(
+                    text: ".",
+                    style: TextStyle(
+                      color: Colors.blue[300],
+                    ),
+                  ),
+                  TextSpan(
+                    text: "in",
+                    style: TextStyle(
+                      color: Colors.grey[200],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
       body: Column(
         children: <Widget>[
-          SizedBox(height: 10,),
+          SizedBox(
+            height: 10,
+          ),
           Measures(),
         ],
       ),
