4706dec4d5f14d1f82627af01298c57680ccfb96
Hash sathish <55404795+hashsathish@users.noreply.github.com>
2020-07-09 17:53:43 +0530

    Update page_2.dart

--- lib/pages/page_2.dart
+++ lib/pages/page_2.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import '../widgets/app_bar.dart';
+import '../widgets/top.dart';
 
 class Page2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('PAGE2'),
+    return Scaffold(
+      appBar: homeAppBar(context),
+      body: Column(
+        children: <Widget>[
+          SizedBox(height: 10,),
+          Measures(),
+        ],
+      ),
     );
   }
 }
