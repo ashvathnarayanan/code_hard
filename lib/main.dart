3bd2b0337f85dd6f26f8926373283085acf2a332
ashvathnarayanan <ashvathofficial@gmail.com>
2020-07-15 18:32:08 +0530

    package id changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:version1/quiz/mainQuizPage.dart';
 import './screens/Root.dart';
 
 void main() => runApp(MyApp());
